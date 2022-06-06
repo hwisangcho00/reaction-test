f16a717c9610313944df954601222ad04acd73bb
hwisangcho00 <hwisangcho00@gmail.com>
2022-06-07 00:04:19 +0900

    Edit to html and css file to center the text

--- src/index.js
+++ src/index.js
@@ -10,15 +10,18 @@ if (require('electron-squirrel-startup')) {
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
+    width: 600,
     height: 600,
+
+    autoHideMenuBar: true
+
   });
 
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
